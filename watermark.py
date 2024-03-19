780795436190525a7ea6fd7841f20f5389187834
Alister <alister.david@iqhq.co.uk>
2024-03-19 10:32:38 +0000

    Added a warning when input images are not the same dimension as the watermark iamge

--- watermark.py
+++ watermark.py
@@ -19,45 +19,41 @@ def get_image_orientation(image_path):
         except (AttributeError, KeyError, IndexError):
             pass
         
-    return 0  # Default orientation if EXIF data is not available
+    return 0
 
+def get_image_dimensions(image_path):
+    with Image.open(image_path) as img:
+        return img.size
 
 def watermark(input_image_path, output_image_path, watermark_image_path, position):
-    # Get the orientation of the image
+
     orientation = get_image_orientation(input_image_path)
 
-    # Open the input image
     with Image.open(input_image_path) as base_image:
-        # Rotate the image based on orientation
         base_image = base_image.rotate(orientation, expand=True)
 
-        # Open the watermark image
         with Image.open(watermark_image_path) as watermark:
-            # Convert the watermark image to RGBA mode
             watermark = watermark.convert("RGBA")
-
             width, height = base_image.size
 
-            # Create a transparent output image
+            # Check if watermark dimensions match input image dimensions
+            watermark_width, watermark_height = watermark.size
+            input_dimensions = (width, height)
+            watermark_dimensions = (watermark_width, watermark_height)
+            if input_dimensions != watermark_dimensions:
+                print("Warning: Watermark image dimensions do not match input image dimensions. Watermark may not appear correctly.")
+
             output = Image.new('RGBA', (width, height), (0,0,0,0))
-
-            # Paste the rotated image onto the output
             output.paste(base_image, (0,0))
-
-            # Paste the watermark onto the output
             output.paste(watermark, position, mask=watermark)
-
-            # Convert the output to RGB and save it
             out = output.convert('RGB')
             out.save(output_image_path)
-
 
 if __name__ == '__main__':
     input_directory = "input"
     output_directory = "output"
     watermark_image_directory = "logos"
 
-    # Get the watermark image file from the logos folder
     watermark_image_files = [f for f in os.listdir(watermark_image_directory) if f.endswith(('.png', '.PNG', '.jpg', '.jpeg', '.JPG', '.JPEG'))]
     if len(watermark_image_files) != 1:
         raise ValueError("There should be exactly one image file in the logos folder.")
@@ -71,3 +67,5 @@ if __name__ == '__main__':
             watermark(input_image_path, output_image_path, watermark_image_path, (0,0))
             print(f"{count}: {filename} watermarked!")
             count += 1
+            
+# Usage: python3 watermark.py
